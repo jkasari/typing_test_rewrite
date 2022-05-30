d02b2d8431027beb4c28ec96e53c9c7f6d7cf9f9
jkasari <jkasari@outlook.com>
2022-05-30 15:36:01 -0700

    Well it kinda does something

--- main.py
+++ main.py
@@ -20,27 +20,32 @@ class MainFrame(wx.Frame):
         self.init_panel(size)
         self.init_sizer()
         self.func_panel.init_actions()
-        self.func_panel.change_prompt("BUTTS")
+        self.prompt_string = ""
+        self.action_control("RESTART_DISPLAY")
         self.frame_sizer.Layout()
 
     def init_panel(self, size: tuple):
         self.func_panel = FuncPanel(self, size)
+    
+    def restart_display(self):
+        self.func_panel.input_box.Hide()
+        self.func_panel.restart_button.Show()
 
+    def start_game(self):
+        self.func_panel.restart_button.Hide()
+        self.func_panel.input_box.Show()
+        self.func_panel.update_prompt_text(self.prompt_string)
+
+    # This controls all actions that require a widget to change on the panel.
     def action_control(self, action: str):
-        if action == "test":
-            #self.func_panel.change_prompt("BUTTS")wx.statictext
-            pass
-        if action == "check input":
-            self.func_panel.change_prompt("BUTTS")
+        if action == "RESTART_DISPLAY":
+            self.restart_display()
+        if action == "CHECK_INPUT":
+            self.func_panel.update_prompt_text(self.prompt_string)
+        if action == "START_GAME":
+            self.prompt_string = "Start"
+            self.start_game()
         self.frame_sizer.Layout()
-
-    def read_input(self):
-        temp = self.func_panel.input_box.GetValue()
-        ans = {}
-        for char in "BUTTS":
-            ans[char] = False
-        return ans
-
 
     def init_sizer(self):
         self.frame_sizer = wx.BoxSizer(wx.VERTICAL)
@@ -64,7 +69,7 @@ class FuncPanel(wx.Panel):
         self.test_count = 0
 
     def init_restart_button(self):
-        self.restart_button = wx.Button(self, label='', pos=(270, 370), size=(90, 30))
+        self.restart_button = wx.Button(self, label='Restart', pos=(270, 370), size=(90, 30))
         self.restart_button.Hide()
 
     
@@ -83,19 +88,32 @@ class FuncPanel(wx.Panel):
   
 
     def init_actions(self):
-        self.Bind(wx.EVT_BUTTON, self.restart_pushed)
-        self.Bind(wx.EVT_TEXT, self.text_entered)
+        self.Bind(wx.EVT_BUTTON, self.restart_pushed, self.restart_button)
+        self.Bind(wx.EVT_TEXT, self.text_entered, self.input_box)
 
+    # Tell action control it needs to potentially update the prompt.
     def text_entered(self, e):
-        self.parent.action_control("check input")
+        #self.parent.action_control("CHECK_INPUT")
+        self.parent.action_control("RESTART_DISPLAY")
 
+    # Tell action control to restart the app.
     def restart_pushed(self, e):
-        self.parent.action_control("test")
+        self.parent.action_control("START_GAME")
 
-    def write_prompt_text(self, text: str):
+    # Takes a string and displays it as the prompt. Everytime it is called it checks the input box and either color codes the prompt or asks for it to be changed. 
+    def update_prompt_text(self, text: str):
+        if self.prompt_sizer.GetChildren():
+            self.clear_prompt()
         input = ""
         if self.input_box.GetValue():
             input = self.input_box.GetValue()
+        if input == text:
+            self.parent.action_control("RESTART_DISPLAY")
+        else:
+            self.write_prompt_text(text, input)
+
+    # Takes in a string for the prompt and for the input. It then fills the prompt sizer with color coded characters based on which ones match the input string.
+    def write_prompt_text(self, text: str, input: str):
         for int, char in enumerate(text):
             prompt_char = wx.StaticText(self, label=char, style=wx.ALIGN_CENTER)
             #prompt_font = wx.Font(pointSize= 60, family=wx.FONTFAMILY_DEFAULT, style=wx.FONTSTYLE_MAX,  weight=wx.FONTWEIGHT_NORMAL)
@@ -106,17 +124,13 @@ class FuncPanel(wx.Panel):
                 if input[int] != char:
                     prompt_char.SetForegroundColour((255, 0, 0))
             self.prompt_sizer.Add(prompt_char)
-                    
 
+    # A helper function that clears the prompt of all its chilren
     def clear_prompt(self):
         for i in reversed(range(len(self.prompt_sizer.GetChildren()))):
             self.prompt_sizer.Hide(i)
             self.prompt_sizer.Remove(i)
 
-    def change_prompt(self, text: str):
-        if self.prompt_sizer.GetChildren():
-            self.clear_prompt()
-        self.write_prompt_text(text)
         
 
 
